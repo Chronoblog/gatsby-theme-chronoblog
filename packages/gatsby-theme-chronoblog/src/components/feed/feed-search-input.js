4efc9d88377774bc8ce22110c92b345548d42658
Ivan Ganev <iganevru@gmail.com>
2019-08-10 05:10:58 +0300

    @theme-ui/preset-base

--- packages/gatsby-theme-chronoblog/src/components/feed/feed-search-input.js
+++ packages/gatsby-theme-chronoblog/src/components/feed/feed-search-input.js
@@ -4,12 +4,13 @@ import { jsx } from 'theme-ui';
 export default ({ value, onChange }) => {
   return (
     <input
-      // sx={{
-      //   bg: 'muted',
-      //   px: 2,
-      //   py: 2,
-      //   width: ['100%', '50%']
-      // }}
+      sx={{
+        color: 'inherit',
+        bg: 'inherit',
+        px: 2,
+        py: 2,
+        width: ['100%', '50%']
+      }}
       placeholder="search"
       value={value}
       onChange={onChange}
--- packages/gatsby-theme-chronoblog/src/gatsby-plugin-theme-ui/theme.js
+++ packages/gatsby-theme-chronoblog/src/gatsby-plugin-theme-ui/theme.js
@@ -1,4 +1,4 @@
-import { base } from '@theme-ui/presets';
+import base from '@theme-ui/preset-base';
 
 import colors from './colors';
 import styles from './styles';
