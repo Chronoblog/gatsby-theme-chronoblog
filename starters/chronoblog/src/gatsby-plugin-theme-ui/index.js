96d4deb13477f1e15bd11350c8326e62504ff270
Ivan Ganev <iganevru@gmail.com>
2019-09-07 09:04:37 +0300

    more style in starter

--- starters/chronoblog/src/gatsby-plugin-theme-ui/index.js
+++ starters/chronoblog/src/gatsby-plugin-theme-ui/index.js
@@ -2,9 +2,23 @@ import chronoblogTheme from 'gatsby-theme-chronoblog/src/gatsby-plugin-theme-ui'
 
 export default {
   ...chronoblogTheme,
+  initialColorMode: 'light',
   colors: {
     ...chronoblogTheme.colors,
     primary: '#3a5f7d',
-    secondary: '#5a809e'
+    secondary: '#5a809e',
+    muted: '#dae1e3',
+    modes: {
+      dark: {
+        ...chronoblogTheme.colors.modes.dark,
+        muted: '#161b1d'
+      }
+    }
+  },
+  fonts: {
+    ...chronoblogTheme.fonts,
+    body: '-apple-system,BlinkMacSystemFont,Helvetica,Arial,sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace'
   }
 };
