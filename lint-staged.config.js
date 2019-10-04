1b452f2822f933a552d6909bcd4aba648da11b0d
Ivan Ganev <iganevru@gmail.com>
2019-10-04 19:04:34 +0300

    lerna

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [
+    `eslint --ignore-path .gitignore --ignore-path .prettierignore --fix`,
+    `git add`
+  ],
+  '*.{md,mdx,json}': [`prettier "**/*.{md,mdx,json}" --write`, `git add`]
+};
