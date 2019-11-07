ab2d6a0d623b1a9e8a7ccddd88b2a067cef968fb
Ivan Ganev <iganevru@gmail.com>
2019-11-07 21:54:43 +0300

    header

--- packages/gatsby-theme-chronoblog/src/components/header.js
+++ packages/gatsby-theme-chronoblog/src/components/header.js
@@ -1,12 +1,10 @@
 /** @jsx jsx */
 import { Container, Flex, Header, jsx } from 'theme-ui';
 
-import useSiteMetadata from '../hooks/use-site-metadata';
 // @ts-ignore
 import SiteHeader from '../site-header.mdx';
 
 export default () => {
-  const siteMetadata = useSiteMetadata();
   return (
     <Header>
       <Container sx={{ marginBottom: ['5px'] }}>
@@ -15,7 +13,7 @@ export default () => {
             flexWrap: 'wrap'
           }}
         >
-          <SiteHeader siteMetadata={siteMetadata} />
+          <SiteHeader />
         </Flex>
       </Container>
     </Header>
