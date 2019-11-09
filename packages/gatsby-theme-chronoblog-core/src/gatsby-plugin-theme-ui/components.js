7e02d81ad29fd3e74060807d8586c1fac66ccc5f
Ivan Ganev <iganevru@gmail.com>
2019-11-09 04:47:33 +0300

    del H tag

--- packages/gatsby-theme-chronoblog-core/src/gatsby-plugin-theme-ui/components.js
+++ packages/gatsby-theme-chronoblog-core/src/gatsby-plugin-theme-ui/components.js
@@ -2,16 +2,7 @@
 import Prism from '@theme-ui/prism';
 import { jsx } from 'theme-ui';
 
-const heading = (Tag) => ({ children, ...props }) => {
-  return <Tag {...props}>{children}</Tag>;
-};
-
 export default {
-  h2: heading('h2'),
-  h3: heading('h3'),
-  h4: heading('h4'),
-  h5: heading('h5'),
-  h6: heading('h6'),
   pre: (props) => props.children,
   code: Prism
 };
