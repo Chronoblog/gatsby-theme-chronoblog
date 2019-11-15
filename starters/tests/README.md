53297934a3ea7e0e47af6b69b8087b3e8b99af11
Ivan Ganev <iganevru@gmail.com>
2019-11-15 17:07:51 +0300

    tests starters

---
+++ starters/tests/README.md
@@ -0,0 +1,3 @@
+# gatsby-starter-tests
+
+[Chronoblog Gatsby Theme](https://github.com/Ganevru/gatsby-theme-chronoblog).
---
+++ starters/tests/gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: 'Chronoblog Tests',
+    siteUrl: 'https://chronoblog-tests.netlify.com/'
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-theme-chronoblog',
+      options: {}
+    }
+  ]
+};
