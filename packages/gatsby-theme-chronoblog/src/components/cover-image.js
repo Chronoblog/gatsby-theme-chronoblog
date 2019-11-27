8b5b857accd9d07932bc3a8f5af036e06586d600
Ivan Ganev <iganevru@gmail.com>
2019-11-27 13:47:18 +0300

    image backdrop for Firefox

--- packages/gatsby-theme-chronoblog/src/components/cover-image.js
+++ packages/gatsby-theme-chronoblog/src/components/cover-image.js
@@ -1,11 +1,18 @@
 /** @jsx jsx */
 import GatsbyImage from 'gatsby-image';
 import get from 'lodash/get';
+import { isFirefox } from 'react-device-detect';
 import { jsx } from 'theme-ui';
 
 const Image = (props) => <GatsbyImage {...props} />;
 
-const CoverImageBase = ({ data, type, height, coverFluidImage }) => {
+const CoverImageBase = ({
+  data,
+  type,
+  height,
+  coverFluidImage,
+  backdropType = `blurImage`
+}) => {
   const borderRadiusForCard =
     type === 'card'
       ? {
@@ -13,6 +20,15 @@ const CoverImageBase = ({ data, type, height, coverFluidImage }) => {
           borderBottomRightRadius: 0
         }
       : {};
+  //
+  const blurImageStyle = {
+    backdropFilter: `drop-shadow(0px 0px 20px black)`,
+    boxShadow: 'inset 0px 0px 15px black',
+    backgroundImage: `url(${coverFluidImage.src})`,
+    backgroundSize: 'cover'
+  };
+  let backdropStyle = {};
+  if (backdropType === `blurImage`) backdropStyle = blurImageStyle;
   //
   const imageTitle = get(data, 'frontmatter.title', '');
   //
@@ -25,11 +41,7 @@ const CoverImageBase = ({ data, type, height, coverFluidImage }) => {
       <div
         sx={{
           maxHeight: height,
-          backdropFilter: `drop-shadow(0px 0px 20px black)`,
-          boxShadow: 'inset 0px 0px 15px black',
-          // backgroundColor: 'green',
-          backgroundImage: `url(${coverFluidImage.src})`,
-          backgroundSize: 'cover',
+          ...backdropStyle,
           backgroundPosition: 'center',
           borderRadius: 'card',
           ...borderRadiusForCard
@@ -37,8 +49,8 @@ const CoverImageBase = ({ data, type, height, coverFluidImage }) => {
       >
         <Image
           sx={{
-            backdropFilter: `blur(5px) contrast(50%)`,
-            WebkitBackdropFilter: `blur(5px) contrast(50%)`,
+            backdropFilter: 'blur(5px) contrast(50%)',
+            WebkitBackdropFilter: 'blur(5px) contrast(50%)',
             maxHeight: height,
             minHeight: height,
             borderRadius: 'card',
@@ -66,6 +78,9 @@ export default ({ data, type = 'post' }) => {
   );
   if (!coverFluidImage) return <div />;
   //
+  let backdropType = 'blurImage';
+  if (isFirefox) backdropType = 'none';
+  //
   const heightMain = 366;
   const heightMobile = 183;
   const heightArray = [heightMobile, heightMain];
@@ -76,6 +91,7 @@ export default ({ data, type = 'post' }) => {
       type={type}
       height={heightArray}
       coverFluidImage={coverFluidImage}
+      backdropType={backdropType}
     />
   );
 };
