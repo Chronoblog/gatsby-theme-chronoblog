aa6409848ae6a50cbfc5087232d48339f77278d9
Ivan Ganev <iganevru@gmail.com>
2019-11-30 04:50:08 +0300

    author-banner fix

--- packages/gatsby-theme-chronoblog/src/components/author-banner.js
+++ packages/gatsby-theme-chronoblog/src/components/author-banner.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
-import { Avatar, Text } from '@theme-ui/components';
-import { jsx, Styled } from 'theme-ui';
+import { Avatar, Heading, Text } from '@theme-ui/components';
+import { jsx } from 'theme-ui';
 
 import useSiteMetadata from '../hooks/use-site-metadata';
 import SocialLinks from './social-links';
@@ -14,37 +14,36 @@ const AuthorBannerMain = ({
   ...props
 }) => {
   return (
-    <div sx={{ marginY: '40px' }} {...props}>
+    <div
+      sx={{
+        bg: 'muted',
+        borderRadius: 'authorBanner',
+        px: ['6px', '36px'],
+        pt: '36px',
+        pb: '36px'
+      }}
+      {...props}
+    >
       <div
         sx={{
-          bg: 'muted',
-          borderRadius: 'authorBanner',
-          px: ['6px', '36px'],
-          pt: '36px',
-          pb: '36px'
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center'
         }}
       >
-        <div
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            alignItems: 'center'
-          }}
-        >
-          {avatar ? <Avatar sx={{ marginRight: '30px' }} src={avatar} /> : ''}
-          <div>
-            <Styled.h4>{author}</Styled.h4>
-            <Text sx={{ fontSize: [3], mb: '8px' }}>{authorDescription}</Text>
-            {socialLinks && socialLinks.length > 0 ? (
-              <SocialLinks socialLinks={socialLinks} fontSize="30px" />
-            ) : (
-              ''
-            )}
-          </div>
+        {avatar ? <Avatar sx={{ marginRight: '30px' }} src={avatar} /> : ''}
+        <div>
+          <Heading>{author}</Heading>
+          <Text sx={{ fontSize: [3], mb: '8px' }}>{authorDescription}</Text>
+          {socialLinks && socialLinks.length > 0 ? (
+            <SocialLinks socialLinks={socialLinks} fontSize="30px" />
+          ) : (
+            ''
+          )}
         </div>
-        <div sx={{ px: ['6px', '6px'] }}>
-          <div>{children}</div>
-        </div>
+      </div>
+      <div sx={{ px: ['6px', '6px'] }}>
+        <div>{children}</div>
       </div>
     </div>
   );
