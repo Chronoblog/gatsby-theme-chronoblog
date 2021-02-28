d91e4fbdc4b822d8a92f94f77d4be7f4e7e1af95
Ivan Ganev <8168280+Ganevru@users.noreply.github.com>
2021-02-28 21:32:35 +0300

    upd generate-starters script

--- scripts/generate-starters/index.js
+++ scripts/generate-starters/index.js
@@ -1,9 +1,37 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 
-console.log('Start Starters Generator');
+const getStartersList = async () => {
+  return fs
+    .readdirSync(`./scripts/generate-starters/starters`)
+    .map((folderName) => folderName);
+};
 
-const startersList = fs
-  .readdirSync(`./scripts/generate-starters/starters`)
-  .map((folder) => {
-    return folder;
+const copyFolder = async (folderThatCopied = '/', folderWhereCopy = '/') => {
+  try {
+    await fs.copy(folderThatCopied, folderWhereCopy);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const generateStarters = async () => {
+  console.log('Start Starters Generator');
+
+  const startersList = await getStartersList();
+
+  startersList.map(async (folderName) => {
+    fs.rmdirSync(`./starters/${folderName}`, { recursive: true });
+    await copyFolder(
+      './scripts/generate-starters/base-starter',
+      `./starters/${folderName}`
+    );
+    await copyFolder(
+      './scripts/generate-starters/starters/chronoblog',
+      `./starters/${folderName}`
+    );
   });
+
+  console.log('End Starters Generator');
+};
+
+generateStarters();
