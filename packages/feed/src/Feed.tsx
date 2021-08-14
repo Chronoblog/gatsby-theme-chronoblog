1ef7674f2fdab285c83fce9a3abb4ad80d54165c
Ivan Ganev <ganevio92@gmail.com>
2021-08-15 01:53:17 +0300

    setup ts

---
+++ packages/feed/src/Feed.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Feed = () => {
+  return <div>Feed</div>
+}
+
+export {Feed}
--- test-builds/next-test-build-chronoblog/pages/index.tsx
+++ test-builds/next-test-build-chronoblog/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Feed from '@chronoblog/feed'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Feed />
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
