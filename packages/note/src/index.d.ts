5dd305d59fa82a7446a8684847b63995f2d2fb90
Ivan Ganev <ganevio92@gmail.com>
2021-08-15 17:06:12 +0300

    cleaning

--- packages/note/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Note } from './Note';
-export default Note;
--- packages/note/src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Note_1 = require("./Note");
-exports.default = Note_1.Note;
