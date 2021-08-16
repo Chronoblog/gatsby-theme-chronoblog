f742d1cf21f37e68a563e6df0f86b56f193b3c89
Ivan Ganev <ganevio92@gmail.com>
2021-08-16 03:53:04 +0300

    try @headlessui/react

--- test-builds/next-app/pages/index.js
+++ test-builds/next-app/pages/index.js
@@ -1,9 +1,11 @@
 import Feed from '@chronoblog/feed'
+import Menu from '../components/Menu'
 
 export default function Home() {
   return (
     <div className="container mx-auto px-4">
       <Feed />
+      <Menu />
     </div>
   )
 }
