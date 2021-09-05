560f3c858da01b06b64a6759d9f2b859d1ba8fae
Ivan Ganev <ganevio92@gmail.com>
2021-09-06 02:54:31 +0300

    use ts in next-app-tw

--- test-builds/next-app-tw/components/Test.js
+++
@@ -1,7 +0,0 @@
-import { TestRender } from '@chronoblog/test-headlessui'
-
-const Test = () => {
-  return <div className="text-8xl"><TestRender /></div>
-}
-
-export { Test }
---
+++ test-builds/next-app-tw/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="bg-blue bg-opacity-5">
+      <div className="container mx-auto">
+        <h2>test</h2>
+      </div>
+    </div>
+  )
+}
