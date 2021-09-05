d040ff0423d86477d2447d8f0e19ef0fe610e2a1
Ivan Ganev <ganevio92@gmail.com>
2021-09-05 22:13:40 +0300

    small

--- test-builds/next-app-tw/pages/index.js
+++ test-builds/next-app-tw/pages/index.js
@@ -3,12 +3,16 @@ import { SwitchTest } from '../components/Switch'
 import Feed from '@chronoblog/feed'
 
 export default function Page() {
-  return <div className="container mx-auto">
-    <div className="">
-      <Feed />
-      <div className="text-blue"><h1>Test Test Test Test Test Test</h1></div>
-      <Test />
-      <SwitchTest />
+  return <>
+    <div className="bg-blue bg-opacity-5">
+      <div className="container mx-auto ">
+        <div className="">
+          <Feed />
+          <div className="text-blue"><h1>Test Test Test Test Test Test</h1></div>
+          <Test />
+          <SwitchTest />
+        </div>
+      </div>
     </div>
-  </div>
+  </>
 }
